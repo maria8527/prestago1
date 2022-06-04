8b66e8ae4494d910e59111dd5760e7442c3b6bf0
sindy silva <sindysilva520@gmail.com>
2022-06-04 18:15:37 -0500

    actualizacion sql

--- src/database/script.sql
+++ src/database/script.sql
@@ -16,11 +16,11 @@ CREATE SEQUENCE registro_seq;
 
 CREATE TABLE registro  (
   id INT4 NOT NULL DEFAULT NEXTVAL('registro_seq'), 
-  N_documento INTEGER(30) NOT NULL,
+  N_documento INTEGER NOT DEFAULT NULL ,
   tipo_documento VARCHAR(4) NOT NULL,
-  nombre_completo VARCHAR(50) NOT NULL,
+  nombre_completo VARCHAR(200) NOT NULL,
   fecha_nacimiento date NOT NULL,
-  numero_celular INTEGER(30) NOT NULL,
+  numero_celular INTEGER NOT NULL,
   email VARCHAR(50) NOT NULL,
   Profesión_u_oficio VARCHAR(100) NOT NULL,
   direccion VARCHAR(100) NOT NULL,
@@ -30,20 +30,26 @@ CREATE TABLE registro  (
 )
 
 
+
 CREATE SEQUENCE prestamo_seq;
 
 CREATE TABLE prestamo  (
   id INT4 NOT NULL DEFAULT NEXTVAL('prestamo_seq'), 
-  monto_prestar INTEGER(30) NOT NULL,
-  plazo_en_meses INTEGER(4) NOT NULL,
-  nombre_completo VARCHAR(50) NOT NULL,
+  monto_prestar INTEGER NOT NULL,
+  plazo_en_meses INTEGER NOT NULL,
+  nombre_completo VARCHAR(200) NOT NULL,
   fecha_creacion timestamp NOT NULL DEFAULT current_timestamp,
-  tasa_interes INTEGER(6) NOT NULL,
+  tasa_interes INTEGER NOT NULL,
   estado VARCHAR(20) NOT NULL,
   PRIMARY KEY (id),
-    CONSTRAINT FK_registro FOREIGN KEY (id)
+    CONSTRAINT FK_registro FOREIGN KEY (regitro_id)
      REFERENCES registro (id) ON DELETE RESTRICT ON UPDATE CASCADE
 )
+
+INSERT INTO prestamo VALUES (NEXTVAL('prestamo_seq') '5000000', '12', 'sindy silva ','2022-01-15', '0,18', 'aprobado',);
+INSERT INTO prestamo VALUES (NEXTVAL('prestamo_seq') '6000000', '24', 'maria alejandra trujillo','2022-01-20', '0,15', 'aprobado',);
+INSERT INTO prestamo VALUES (NEXTVAL('prestamo_seq') '5000000', '36', 'juliana sanchez','2021-09-15', '0,20', 'aprobado',);
+
 
 
 CREATE SEQUENCE pago_seq;
@@ -51,16 +57,19 @@ CREATE SEQUENCE pago_seq;
 CREATE TABLE pago  (
   id INT4 NOT NULL DEFAULT NEXTVAL('pago_seq'), 
   fecha_pago_cuotas date NOT NULL,
-  tiempo_pagar INTEGER(10) NOT NULL,
-  cuota_pagar INTEGER(100) NOT NULL,
+  tiempo_pagar INTEGER NOT NULL,
+  cuota_pagar INTEGER NOT NULL,
 
   PRIMARY KEY (id),
    CONSTRAINT fk_prestamo 
-     FOREIGN KEY (id) REFERENCES prestamo (id) 
+     FOREIGN KEY (prestamo_id) REFERENCES prestamo (id) 
      ON DELETE RESTRICT ON UPDATE CASCADE,
-     CONSTRAINT fk_registro FOREIGN KEY (id) 
+     CONSTRAINT fk_registro FOREIGN KEY (registro_id) 
      REFERENCES registro (id) ON DELETE RESTRICT ON UPDATE CASCADE
 )
+INSERT INTO pago VALUES (NEXTVAL('pago_seq') '2022-02-15', '12', '1',);
+INSERT INTO pago VALUES (NEXTVAL('pago_seq') '2022-02-20', '24',  '1',);
+INSERT INTO pago VALUES (NEXTVAL('paho_seq') '2022-05-15', '36', '8',);
 
 
 CREATE SEQUENCE historial_seq;
@@ -69,8 +78,10 @@ CREATE TABLE historial  (
   
   PRIMARY KEY (id),
   CONSTRAINT fk_prestamo 
-  FOREIGN KEY (id) REFERENCES prestamo (id) 
+  FOREIGN KEY (prestamo_id) REFERENCES prestamo (id) 
   ON DELETE RESTRICT ON UPDATE CASCADE,
  CONSTRAINT fk_registro FOREIGN KEY (id) 
  REFERENCES registro (id) ON DELETE RESTRICT ON UPDATE CASCADE
 )
+
+
